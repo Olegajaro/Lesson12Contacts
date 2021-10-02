533ba5c9495148cc503e715d6fdafc5ec722b2ee
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-02 18:07:38 +0300

    Starter version app

--- Lesson12Contacts/Viewcontrollers/NewContactViewController.swift
+++ Lesson12Contacts/Viewcontrollers/NewContactViewController.swift
@@ -9,10 +9,38 @@ import UIKit
 
 class NewContactViewController: UIViewController {
 
+    @IBOutlet var doneButton: UIBarButtonItem!
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    
+    var delegate: NewContactViewControllerDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        firstNameTextField.addTarget(
+            self,
+            action: #selector(firstNameTextFieldDidChanged),
+            for: .editingChanged)
     }
     
-
+    @IBAction func doneButtonPressed(_ sender: Any) {
+        saveAndExit()
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    @objc private func firstNameTextFieldDidChanged() {
+        guard let firstName = firstNameTextField.text else { return }
+        doneButton.isEnabled = !firstName.isEmpty
+    }
+    
+    private func saveAndExit() {
+        guard let firstName = firstNameTextField.text else { return }
+        guard let lastName = lastNameTextField.text else { return }
+        delegate.saveContact("\(firstName) \(lastName)")
+        dismiss(animated: true)
+    }
 }
