526b30f14d2efff6c4464abba7feae15b63e4b96
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-10-02 20:49:33 +0300

    storing objects in plist

--- Lesson12Contacts/Services/StorageManager.swift
+++ Lesson12Contacts/Services/StorageManager.swift
@@ -13,22 +13,28 @@ class StorageManager {
     
     private let userDefaults = UserDefaults.standard
     private let key = "contacts"
+    private let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    private let archiveURL: URL
     
-    private init() {}
+    private init() {
+        archiveURL = documentDirectory.appendingPathComponent("Contacts").appendingPathExtension("plist")
+    }
 
     // метод для добавление объекта в массив сохраняемых контактов
     // для начала восстанавливаем массив
     func save(contact: Contact) {
         var contacts = fetchContacts()
         contacts.append(contact)
-        guard let data = try? JSONEncoder().encode(contacts) else { return }
-        userDefaults.set(data, forKey: key)
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+//        userDefaults.set(data, forKey: key)
+        try? data.write(to: archiveURL, options: .noFileProtection)
     }
     
     // метод для загрузки данных
     func fetchContacts() -> [Contact] {
-        guard let data = userDefaults.data(forKey: key) else { return [] }
-        guard let contacts = try? JSONDecoder().decode([Contact].self, from: data) else { return [] }
+//        guard let data = userDefaults.data(forKey: key) else { return [] }
+        guard let data = try? Data(contentsOf: archiveURL) else { return [] }
+        guard let contacts = try? PropertyListDecoder().decode([Contact].self, from: data) else { return [] }
         
         return contacts
     }
@@ -40,7 +46,9 @@ class StorageManager {
     func deleteContact(at index: Int) {
         var contacts = fetchContacts()
         contacts.remove(at: index)
-        guard let data = try? JSONEncoder().encode(contacts) else { return }
-        userDefaults.set(data, forKey: key)
+//        guard let data = try? JSONEncoder().encode(contacts) else { return }
+//        userDefaults.set(data, forKey: key)
+        guard let data = try? PropertyListEncoder().encode(contacts) else { return }
+        try? data.write(to: archiveURL, options: .noFileProtection)
     }
 }
